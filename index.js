724a42d38a22d87f32aceb33a572a5d7ff26ec45
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-02 17:58:01 -0600

    commit tree 74de89ff5f6d0358cec889bc8ceda0e0dda496f0

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'kyle'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var customerName = 'bob'
+
+// function upperCaseCustomerName() {
+//     return customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = 'not bob';
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = 'maybe bob'
+// }
+
+// const leastFavoriteCustomer = 'Smelly Old Man'
+
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = 'JJs'
+// }
